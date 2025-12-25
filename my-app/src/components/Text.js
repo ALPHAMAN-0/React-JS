02a3b4bdd039d41aefed118af77834a59bc196ff
SIAM HOSSAIN <huffmancoding.acm@gmail.com>
2025-12-25 19:18:34 +0600

    ss

--- my-app/src/components/Text.js
+++ my-app/src/components/Text.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export default function Text(props) {
+    
     const { text, setText } = props;
     
     function capitalizeName(name) {
-     return name
-     .toLowerCase()
-     .split(" ")
-     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-     .join(" ");
+      return name
+      .toLowerCase()
+      .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
     }
 
     const handleUpClick = ()=>{
@@ -16,22 +17,26 @@ export default function Text(props) {
         let newText = capitalizeName(text);
         setText(newText);
     }
+
     const handleOnChange = (event)=>{
-        console.log("On Change");
-        
+        console.log("On Change");    
         setText(event.target.value);
     }
+
     return (
     <>
         <h1>{props.heading}</h1>
 
         <div className="form-floating">
+        
         <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "500px"}} value={text} onChange={handleOnChange}></textarea>
          
         <label htmlFor="floatingTextarea2">please enter your text here</label>
+        
         </div>
 
         <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
     </> 
   )
+
 }
